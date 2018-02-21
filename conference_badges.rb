be22f944aa77dbb2937411751308fbabde2418e3
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-02-21 23:18:21 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |name|
+    badges << badge_maker(name)
+  end
+  badges
+end
+
+def assign_rooms(attendees)
+  room_assignments = []
+  
+end
