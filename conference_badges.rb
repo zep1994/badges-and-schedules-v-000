7d4e51aaade44a1ab0aee6f52c249a1a6c68484b
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-02-21 23:26:00 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 def assign_rooms(attendees)
   room_assignments = []
   attendees.each_with_index { |attendees, index|
-  room_assignments <<  "Hello, #{attendees}! You'll be assigned to #{index + 1}!"
+  room_assignments <<  "Hello, #{attendees}! You'll be assigned to room #{index + 1}!"
    }
    room_assignments
 end
